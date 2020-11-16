fb74940c2a7c69de924fdf778cf0c3302a7dc269
frankbastidas <fgbcuya@inf.ufrgs.br>
2020-11-15 22:35:48 -0300

    firstPerson

--- Assets/MouseLook.cs
+++ Assets/MouseLook.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+
+    public float mouseSensitivity = 100f;
+
+    public Transform bodyReference;
+    float xRotation = 0f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        //Debug.Log((int)(1f / Time.unscaledDeltaTime));
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        bodyReference.Rotate(Vector3.up * mouseX);
     }
 }
